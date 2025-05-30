fded7f5e18b2ed2a969bebb28928c8b560ee9788
Joykinuthia <joyrosekinuthia@gmail.com>
2025-05-30 13:27:10 +0300

    Fixed errors

--- code_challenge/tests/test_article.py
+++ code_challenge/tests/test_article.py
@@ -1,5 +1,5 @@
 import pytest
-from code_challenge.lib.models.article import Article
+from code_challenge.article import Article
 from code_challenge.lib.db.connection import get_connection
 from code_challenge.lib.db.seed import seed_data
 from code_challenge.scripts.setup_db import setup_database
